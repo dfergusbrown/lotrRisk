75d75b15a4319aaf92031e97aec0a71838b863be
dfergusbrown <dfergusbrown@gmail.com>
2024-03-28 21:51:29 -0400

    zoom controls working

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Helpful maps:
 
 http://lotrproject.com/map/#zoom=4&lat=-1693.5&lon=1500&layers=BTTTTTTTT
+
+image map creator
+https://www.image-map.net/
--- script.js
+++ script.js
@@ -2,14 +2,20 @@ const mapImage = document.getElementById('map-image');
 const zoomInButton = document.getElementById('zoom-in');
 const zoomOutButton = document.getElementById('zoom-out');
 
-let zoomLevel = 100; // Initial zoom level (adjust as needed)
+mapImage.style.transformOrigin = '0 0'; // Set origin to top-left corner
 
+let zoomLevel = 100; // Initial zoom level (adjust as needed)
+console.log(zoomLevel)
 zoomInButton.addEventListener('click', () => {
-  zoomLevel += 30; // Adjust zoom increment value
-  mapImage.style.transform = `scale(${zoomLevel}%)`;
+    zoomLevel = Math.min(zoomLevel + 20, 160); // Limit zoom to 100%
+    mapImage.style.transform = `scale(${zoomLevel}%)`
+    console.log(zoomLevel);
 });
 
 zoomOutButton.addEventListener('click', () => {
-  zoomLevel -= 30;
-  mapImage.style.transform = `scale(${zoomLevel}%)`;
+    zoomLevel = Math.max(zoomLevel - 20, 100); // Limit zoom to 100%
+    mapImage.style.transform = `scale(${zoomLevel}%)`
+    console.log(zoomLevel);
 });
+
+// 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
